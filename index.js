28469cb9de0550f0a159ce9bd069b805e275640c
Urip Subekti <61002089+subekti404dev@users.noreply.github.com>
2023-12-15 04:38:25 +0000

    fix: fix gha

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const main = async () => {
 
     if (type === 'dotenv') {
       for (const line of (data || '').split('\n')) {
-        const [key] = line.split('=')[0];
+        const key = line.split('=')[0];
         const value = line.replace(key, '');
-        execSync(`::add-mask::${value}`);
+        execSync(`echo "::add-mask::${value}"`);
         execSync(`echo "${key}=${value}" >> $GITHUB_ENV`);
       }
     }
